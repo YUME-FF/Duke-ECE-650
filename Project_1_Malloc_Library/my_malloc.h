195913e655964a60a4a43e8f4accdfd3598b6328
YUME-FF <zf70@duke.edu>
2023-01-17 14:04:07 -0500

    init

--- Project_1_Malloc_Library/my_malloc.h
+++ Project_1_Malloc_Library/my_malloc.h
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 //First Fit malloc/free
-void *ff_malloc(size_t size);
-void ff_free(void *ptr);
+void * ff_malloc(size_t size);
+void ff_free(void * ptr);
 //Best Fit malloc/free
-void *bf_malloc(size_t size);
-void bf_free(void *ptr);
+void * bf_malloc(size_t size);
+void bf_free(void * ptr);
