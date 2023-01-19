c3323351f7843e09837816864a087db3b5243d37
YUME-FF <zf70@duke.edu>
2023-01-19 14:34:09 -0500

    add remove_chunk

--- Project_1_Malloc_Library/my_malloc.c
+++ Project_1_Malloc_Library/my_malloc.c
@@ -66,20 +66,20 @@ Function: remove targeted chunk
 */
 void remove_chunk(chunk * ptr) {
   if (free_region_before == ptr) {
-    if (free_region == ptr){
-        free_region = NULL;
-        free_region_before = NULL;
-     }
-    else{
+    if (free_region == ptr) {
+      free_region = NULL;
+      free_region_before = NULL;
+    }
+    else {
       free_region_before = ptr->prev;
       free_region_before->next = NULL;
     }
   }
-  else if (free_region == ptr){
+  else if (free_region == ptr) {
     free_region = ptr->next;
     free_region->prev = NULL;
   }
-  else{
+  else {
     ptr->prev->next = ptr->next;
     ptr->next->prev = ptr->prev;
   }
@@ -106,10 +106,10 @@ void * ff_malloc(size_t size) {
   if (free_region != NULL) {
     chunk * ptr = free_region;
     while (ptr != NULL) {
-      if(ptr->size >= size){ //Start first fit
-        if(ptr->size >= size + META_SIZE){
+      if (ptr->size >= size) {  //Start first fit
+        if (ptr->size >= size + META_SIZE) {
           chunk * split = split_chunk(size, ptr);
-          remove(ptr);
+          remove_chunk(ptr);
           extend_chunk(split);
           ptr->size = size;
           //free_size -= (size + sizeof(Metadata));
@@ -122,7 +122,6 @@ void * ff_malloc(size_t size) {
         ptr->next = NULL;
         ptr->prev = NULL;
         return (char *)ptr + META_SIZE;
-        }
       }
       ptr = ptr->next;
     }
