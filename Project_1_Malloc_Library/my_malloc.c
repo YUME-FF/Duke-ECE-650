b8198f725a49b8d03890d8b1402948b9333a6cf0
Zhe Fan <fzyume@gmail.com>
2023-01-19 11:04:14 -0500

    Update my_malloc.c

--- Project_1_Malloc_Library/my_malloc.c
+++ Project_1_Malloc_Library/my_malloc.c
@@ -3,7 +3,9 @@
 #define META_SIZE sizeof(struct chunk)
 
 chunk * first = NULL;
-
+chunk * free_region = NULL;
+chunk * free_region_before = NULL;
+size_t _size = 0;
 
 
 
@@ -14,6 +16,7 @@ and add new block to the end of the struct chunk.
 */
 void * allocate_space(size_t size){
   chunk * new_block = sbrk(size + META_SIZE);
+  _size += size + META_SIZE; // record the size of data
   if (first == NULL) {
     first = new_block;
   }
@@ -22,4 +25,37 @@ void * allocate_space(size_t size){
   new_block->next = NULL;
   new_block->prev = NULL;
   return (char *)new_block + META_SIZE;
+}
+
+/*
+
+*/
+void add_chunk(chunk * ptr){
+  if (free_region == NULL)|| (ptr < free_region)) {
+    ptr->prev = NULL;
+    ptr->next = free_region;
+    if (ptr->next != NULL) {
+      ptr->next->prev = ptr;
+    }
+    else {
+      free_region_before = ptr;
+    }
+    free_region = ptr;
+  }
+  else {
+    Metadata * curr = free_region;
+    while ((curr->next != NULL) && (ptr > curr->next)) {
+      curr = curr->next;  //curr< ptr< curr->next, keep going
+    }
+    ptr->prev = curr;
+    ptr->next = curr->next;
+    curr->next = ptr;
+    if (ptr->next != NULL) {
+      ptr->next->prev = ptr;
+    }
+    else {
+      free_region_before = ptr;
+    }
+  }
+}
 }
